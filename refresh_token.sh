b37dd8b351b16f8a7b5387ff14497da87cb03e18
Paaaark <hifipark0801@gmail.com>
2025-06-11 22:55:30 +0900

    Refresh Token now fetches Mock Token

--- refresh_token.sh
+++ refresh_token.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-ACCESS_TOKEN=$(curl -s -X POST "https://openapivts.koreainvestment.com:29443/oauth2/tokenP" \
+ACCESS_TOKEN=$(curl -s -X POST "https://openapi.koreainvestment.com:9443/oauth2/tokenP" \
   -H "Content-Type: application/json; charset=UTF-8" \
-  -d "{\"grant_type\":\"client_credentials\",\"appkey\":\"$TradingApiKey\",\"appsecret\":\"$TradingSecretKey\"}" | jq -r '.access_token')
+  -d "{\"grant_type\":\"client_credentials\",\"appkey\":\"$KIS_APP_KEY\",\"appsecret\":\"$KIS_APP_SECRET\"}" | jq -r '.access_token')
+
+MOCK_ACCESS_TOKEN=$(curl -s -X POST "https://openapivts.koreainvestment.com:29443/oauth2/tokenP" \
+  -H "Content-Type: application/json; charset=UTF-8" \
+  -d "{\"grant_type\":\"client_credentials\",\"appkey\":\"$KIS_MOCK_APP_KEY\",\"appsecret\":\"$KIS_MOCK_APP_SECRET\"}" | jq -r '.access_token')
 
 export "KIS_ACCESS_TOKEN=$ACCESS_TOKEN"
+export "KIS_MOCK_ACCESS_TOKEN=$MOCK_ACCESS_TOKEN"
